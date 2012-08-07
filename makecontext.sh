6eae89da0fe6022c227f1ca89273117c3f1355e3
OkawaUki <okawa.yuki@nifty.co.jp>
2012-08-07 14:53:15 +0900

    Update makecontext.sh

--- makecontext.sh
+++ makecontext.sh
@@ -15,7 +15,7 @@ ${BIN_MKDIR} ${PATH_WORKDIR}
 
 ## create archive
 ${BIN_TAR} czf ./contexts/krm/web_root.tar.gz ./contexts/krm/public_html ./contexts/krm/doc ./contexts/krm/data ./contexts/krm/test ./contexts/krm/COPYING
-${BIN_RM} ./contexts/krm/public_html
+${BIN_RM} ./contexts/krm/public_html ./contexts/krm/doc ./contexts/krm/data ./contexts/krm/test ./contexts/krm/COPYING
 ${BIN_TAR} czf ./contexts/krm.tar.gz ./contexts/krm
 ${BIN_RM} ./contexts/krm/
 

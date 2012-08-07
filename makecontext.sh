3ec42ae76a24b9abcbacf19e186b709d777d0513
ysaotome <y@sotm.jp>
2012-08-07 19:34:13 +0900

    Modefied makeconfig.sh and public_html

--- makecontext.sh
+++ makecontext.sh
@@ -4,13 +4,19 @@
 
 BIN_RM="/bin/rm -rf"
 BIN_TAR="/usr/bin/tar"
+BIN_MKDIR="/bin/mkdir"
 BIN_CD="cd"
+
+## make context
 
 ## create archive
 ${BIN_CD} ./contexts/krm/
-${BIN_TAR} czf web_root.tar.gz public_html docs data test COPYING
-${BIN_RM} public_html docs data test COPYING
-${BIN_TAR} czf ../krm.tar.gz web_root.tar.gz nginx.conf
-${BIN_RM} ./contexts/krm/
+${BIN_TAR} czpf ./web_root.tar.gz {public_html,docs,data,test,COPYING}
+${BIN_RM} ./{public_html,docs,data,test,COPYING}
+${BIN_CD} ../
+${BIN_TAR} czpf ./krm.tar.gz ./krm/*
+#${BIN_RM} ./krm
 
 ## end
+
+
